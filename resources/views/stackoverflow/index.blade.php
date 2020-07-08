5d31d5196e5d699baf307f25767717a748564b76
gunaitsugo <gunaitsugo@gmail.com>
2020-07-08 21:36:18 +0700

    use template adminlte

---
+++ resources/views/stackoverflow/index.blade.php
@@ -0,0 +1,24 @@
+@extends('adminlte.master')
+
+@section('content')
+<div class="card">
+    <div class="card-header">
+    <h3 class="card-title">Stackoverflow Indo</h3>
+    </div>
+    <!-- /.card-header -->
+    <div class="card-body">
+    Halo
+    </div>
+    <!-- /.card-body -->
+</div>
+@endsection
+
+@push('scripts')
+<script src="{{ asset('/adminlte/plugins/datatables/jquery.dataTables.js') }}"></script>
+<script src="{{ asset('/adminlte/plugins/datatables-bs4/js/dataTables.bootstrap4.js') }}"></script>
+<script>
+  $(function () {
+    $("#example1").DataTable();
+  });
+</script>
+@endpush
